01f90b2fd916656660f26dba321e657bcb0e71a4
zillurahman10 <zillur.hero40@gmail.com>
2023-07-08 12:55:14 +0600

    Cart all work done

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -15,11 +15,38 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action: PayloadAction<IProduct>) {
-      state.products.push(action.payload);
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (existing) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        existing.quantity = existing.quantity! + 1;
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeOne: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (existing && existing.quantity! > 1) {
+        existing.quantity = existing.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload._id
+      );
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeOne, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
